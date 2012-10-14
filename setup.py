fc1cf524871ff4d541cf9ce493c038fa7c7ba9dc
Andreas Hilboll <andreas@hilboll.de>
2012-10-14 13:06:49 +0200

    ENH: distutils support

---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+*****************************************************************************
+geodas - Geospatial Data Analysis in Python :: Setup Script
+*****************************************************************************
+
+:Author:    Andreas Hilboll <andreas@hilboll.de>
+:Date:      Sun Oct 14 12:39:25 2012
+
+"""
+
+from distutils.core import setup
+setup(name='geodas',
+      version='0.1',
+      description='Geospatial Data Analysis in Python',
+      author='Andreas Hilboll',
+      author_email='andreas@hilboll.de',
+      url='http://andreas-h.github.com/geodas/',
+      packages=['geodas'],
+      )
