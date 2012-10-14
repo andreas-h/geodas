c5f6878f777f0ea7994d324a8596c5b66963bb92
Andreas Hilboll <andreas@hilboll.de>
2012-10-14 13:01:48 +0200

    add empty module

---
+++ geodas/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+*****************************************************************************
+geodas - Geospatial Data Analysis in Python
+*****************************************************************************
+
+:Author:    Andreas Hilboll <andreas@hilboll.de>
+:Date:      Sat Oct 13 21:40:15 2012
+:Version:   0.1
+
+"""
+
+__docformat__ = 'restructuredtext'
+__version__ = '0.1.0'
+
+import pkg_resources
+pkg_resources.require("pandas>=0.9.0")
