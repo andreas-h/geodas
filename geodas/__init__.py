e76bd6a193dd17dd7ea89cef713d1f7a3070e08a
Andreas Hilboll <andreas@hilboll.de>
2012-10-16 14:36:37 +0200

    MAINT: add license information (GPLv3)

--- geodas/__init__.py
+++ geodas/__init__.py
@@ -6,7 +6,24 @@ geodas - Geospatial Data Analysis in Python
 
 :Author:    Andreas Hilboll <andreas@hilboll.de>
 :Date:      Sat Oct 13 21:40:15 2012
+:Website:   http://andreas-h.github.com/geodas/
+:License:   GPLv3
 :Version:   0.1
+:Copyright: (c) 2012 Andreas Hilboll <andreas@hilboll.de>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 """
 
