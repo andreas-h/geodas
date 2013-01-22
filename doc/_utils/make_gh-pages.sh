ebfa78bd28d9345b09293007b3562c12775bee37
Andreas Hilboll <andreas@hilboll.de>
2013-01-22 19:39:20 +0100

    DOC: finally (?) fix gh-pages make target

---
+++ doc/_utils/make_gh-pages.sh
@@ -0,0 +1,14 @@
+git checkout gh-pages
+cd .. && mkdir doc && cd doc/
+git checkout master .
+git reset HEAD
+make clean dirhtml
+find ../ -mindepth 1 -maxdepth 1 | grep -v .git | grep -v ../doc | grep -v .nojekyll | grep -v .gitignore | xargs rm -rf
+mv -fv _build/dirhtml/* ../
+git checkout gh-pages
+git add -Af ../
+git rm -rf .
+cd ..
+git commit -m "Generated gh-pages for `git log master -1 --pretty=short --abbrev-commit`"
+git push github gh-pages --force
+git checkout master
