2ee750a62ca6b59ea9e27603b9f09c0fa6a0716c
Andreas Hilboll <andreas@hilboll.de>
2013-01-25 17:48:30 +0100

    BUG: fix string formatting

--- geodas/plotting.py
+++ geodas/plotting.py
@@ -40,7 +40,8 @@ def pcolormesh(gdata, cbar=True, vmin=None, vmax=None, cmap=None,
     # TODO: support kwargs for basmap instance
     if len(gdata.coordiantes) > 2:
         raise ValueError("You asked me to pcolormesh a dataset with dimension "
-                         "%d, and I don't know how to do that.")
+                         "%d, and I don't know how to do that.",
+                         len(gdata.coordinates))
     from mpl_toolkits.basemap import Basemap
     if ax is None:
         plt.figure()
